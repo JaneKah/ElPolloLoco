cdeab1e9c158cb1f9e98a5311ca22bae68bd747c
JoannaKahloun <101954470+JoannaKahloun@users.noreply.github.com>
2022-08-15 14:39:07 +0200

    Character and background added

---
+++ models/cloud.class.js
@@ -0,0 +1,18 @@
+class Cloud extends MovableObject {
+    y = 50;
+    width = 500;
+    height = 250;
+    
+    constructor(){
+        super().loadImage('img/5_background/layers/4_clouds/1.png');
+
+        this.x = 0 + Math.random() * 500; // Number between 200 and 250;
+       this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.15;
+        }, 1000 / 60);
+    }
+}
